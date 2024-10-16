481f1a00787510aac06dd7d3cf28015f6ef21ece
Aida Neitenbach <aneitenb@c1r4p7.hive.fi>
2024-10-16 17:33:37 +0300

    updating ray_casting

--- ray_casting.c
+++ ray_casting.c
@@ -6,90 +6,96 @@
 /*   By: aneitenb <aneitenb@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/16 14:14:45 by aneitenb          #+#    #+#             */
-/*   Updated: 2024/10/16 14:53:30 by aneitenb         ###   ########.fr       */
+/*   Updated: 2024/10/16 17:33:05 by aneitenb         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub3d.h"
 
-void draw_vertical_line(mlx_image_t *img, int x, int start, int end, uint32_t color)
+void    calculate_step(t_ray *ray, t_player *player)
 {
-    for (int y = start; y < end; y++)
-        mlx_put_pixel(img, x, y, color);
+	if (ray->ray_dir_x < 0)
+	{
+		ray->step_x = -1;
+		ray->side_dist_x = (player->pos_x - ray->map_x) * ray->delta_dist_x;
+	}
+	else
+	{
+		ray->step_x = 1;
+		ray->side_dist_x = (ray->map_x + 1.0 - player->pos_x) * ray->delta_dist_x;
+	}
+	if (ray->ray_dir_y < 0)
+	{
+		ray->step_y = -1;
+		ray->side_dist_y = (player->pos_y - ray->map_y) * ray->delta_dist_y;
+	}
+	else
+	{
+		ray->step_y = 1;
+		ray->side_dist_y = (ray->map_y + 1.0 - player->pos_y) * ray->delta_dist_y;
+	}
 }
 
-void calculate_step_and_side_dist(t_ray *ray, t_player *player)
+void    perform_dda(t_ray *ray, char **map)
 {
-    if (ray->rayDirX < 0)
-    {
-        ray->stepX = -1;
-        ray->sideDistX = (player->posX - ray->mapX) * ray->deltaDistX;
-    }
-    else
-    {
-        ray->stepX = 1;
-        ray->sideDistX = (ray->mapX + 1.0 - player->posX) * ray->deltaDistX;
-    }
-    if (ray->rayDirY < 0)
-    {
-        ray->stepY = -1;
-        ray->sideDistY = (player->posY - ray->mapY) * ray->deltaDistY;
-    }
-    else
-    {
-        ray->stepY = 1;
-        ray->sideDistY = (ray->mapY + 1.0 - player->posY) * ray->deltaDistY;
-    }
+	while (ray->hit == 0)
+	{
+		if (ray->side_dist_x < ray->side_dist_y)
+		{
+			ray->side_dist_x += ray->delta_dist_x;
+			ray->map_x += ray->step_x;
+			ray->side = 0;
+		}
+		else
+		{
+			ray->side_dist_y += ray->delta_dist_y;
+			ray->map_y += ray->step_y;
+			ray->side = 1;
+		}
+		if (map[ray->map_y][ray->map_x] == '1')
+			ray->hit = 1;
+	}
 }
 
-void perform_dda(t_ray *ray, char **map)
+void    calculate_wall(t_ray *ray, t_player *player, int *draw_start, int *draw_end)
 {
-    while (ray->hit == 0)
-    {
-        if (ray->sideDistX < ray->sideDistY)
-        {
-            ray->sideDistX += ray->deltaDistX;
-            ray->mapX += ray->stepX;
-            ray->side = 0;
-        }
-        else
-        {
-            ray->sideDistY += ray->deltaDistY;
-            ray->mapY += ray->stepY;
-            ray->side = 1;
-        }
-        if (map[ray->mapY][ray->mapX] == '1')
-            ray->hit = 1;
-    }
+	int	line_height;
+
+	if (ray->side == 0)
+		ray->perp_wall_dist = (ray->map_x - player->pos_x
+			+ (1 - ray->step_x) / 2) / ray->ray_dir_x;
+	else
+		ray->perp_wall_dist = (ray->map_y - player->pos_y
+			+ (1 - ray->step_y) / 2) / ray->ray_dir_y;
+	line_height = (int)(SCREEN_HEIGHT / ray->perp_wall_dist);
+	*draw_start = -line_height / 2 + SCREEN_HEIGHT / 2;
+	if (*draw_start < 0)
+		*draw_start = 0;
+	*draw_end = line_height / 2 + SCREEN_HEIGHT / 2;
+	if (*draw_end >= SCREEN_HEIGHT)
+		*draw_end = SCREEN_HEIGHT - 1;
 }
 
-void calculate_wall_height(t_ray *ray, t_player *player, int *drawStart, int *drawEnd)
+void    ray_casting(t_game *game)
 {
-    if (ray->side == 0)
-        ray->perpWallDist = (ray->mapX - player->posX + (1 - ray->stepX) / 2) / ray->rayDirX;
-    else
-        ray->perpWallDist = (ray->mapY - player->posY + (1 - ray->stepY) / 2) / ray->rayDirY;
+	t_ray		ray;
+	int			draw_start;
+	int			draw_end;
+	int			x;
+	uint32_t	color;
 
-    int lineHeight = (int)(SCREEN_HEIGHT / ray->perpWallDist);
-    *drawStart = -lineHeight / 2 + SCREEN_HEIGHT / 2;
-    *drawStart = (*drawStart < 0) ? 0 : *drawStart;
-    *drawEnd = lineHeight / 2 + SCREEN_HEIGHT / 2;
-    *drawEnd = (*drawEnd >= SCREEN_HEIGHT) ? SCREEN_HEIGHT - 1 : *drawEnd;
-}
-
-void ray_casting(t_game *game)
-{
-    t_ray ray;
-    int drawStart, drawEnd;
-
-    for (int x = 0; x < SCREEN_WIDTH; x++)
-    {
-        init_ray(&ray, &game->player, x);
-        calculate_step_and_side_dist(&ray, &game->player);
-        perform_dda(&ray, game->map);
-        calculate_wall_height(&ray, &game->player, &drawStart, &drawEnd);
-
-        uint32_t color = (ray.side == 1) ? 0xFF0000FF : 0x00FF00FF;
-        draw_vertical_line(game->img, x, drawStart, drawEnd, color);
-    }
+	x = 0;
+	while (x < SCREEN_WIDTH)
+	{
+		init_ray(&ray, &game->player, x);
+		calculate_step(&ray, &game->player);
+		perform_dda(&ray, game->map);
+		calculate_wall(&ray, &game->player, &draw_start, &draw_end);
+		if (ray.side == 1)
+			color = 0xFF0000FF;  // Red if side is 1
+		else
+			color = 0x00FF00FF;    // otherwise green
+		draw_vertical_line(game->img, x, draw_start, draw_end, color);
+		x++;
+	}
 }
